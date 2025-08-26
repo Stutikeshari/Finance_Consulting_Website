b41c8fe05577df9d3d44222fd35b337c3199b934
Stutikeshari <stutikeshari2@gmail.com>
2025-08-26 10:08:03 +0530

    Final complete responsive website is ready

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // required for next export in Next 13+
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig;
